d4e4e1610fb9d7f0af84142979a58af3b1bfca69
vitalyvitmens <otm_@tut.by>
2024-02-25 17:38:35 +0300

    initial commit

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { Button as Btn } from '../../components/Button'
+import reactLogo from '../../assets/react.svg'
+import './Main.css'
+
+export function Main() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Btn>Кнопка</Btn>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
